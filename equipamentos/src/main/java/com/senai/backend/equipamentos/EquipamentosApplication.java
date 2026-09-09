d9cf9f106651825f71cfc69a37da8783cb6f3402
marloww10 <marlon.2007.franco@gmail.com>
2026-09-08 21:00:45 -0300

    criação do primeiro projeto

---
+++ equipamentos/src/main/java/com/senai/backend/equipamentos/EquipamentosApplication.java
@@ -0,0 +1,13 @@
+package com.senai.backend.equipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipamentosApplication.class, args);
+	}
+
+}
